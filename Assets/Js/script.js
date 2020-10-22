955f8c83d6509261d76d678f4229ddf50b330f7c
ksutton <suttonk@sbcglobal.net>
2020-10-22 16:46:04 -0700

    Completed functionality

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,10 +1,24 @@
-$(".save").on("click",function(){
-  var value=$(this).siblings("textarea").val()
-  var time=$(this).attr("id").split("-")[0]
-  console.log(value,time)
-  localStorage.setItem(time,value)
+$(".save").on("click", function () {
+  var value = $(this).siblings("textarea").val()
+  var time = $(this).attr("id").split("-")[0]
+  console.log(value, time)
+  localStorage.setItem(time, value)
 })
-for (var i=9; i<18; i++) {
-  var data = localStorage.getItem(i)
-  $(`#${i}-txt`).val(data)
+var currentTime = moment()
+$("#currentDay").val(currentTime)
+var time = moment().hours()
+console.log(time)
+for (var i = 9; i < 18; i++) {
+      var data = localStorage.getItem(i)
+      $(`#${i}-txt`).val(data)
+      // console.log(i,time)
+      if (i < time) {
+        $(`#${i}-txt`).addClass("past")
+        // $(`#${i}-txt`).addClass("bg-danger")
+
+      } else if (i == time) {
+        $(`#${i}-txt`).addClass("present")
+      } else {
+        $(`#${i}-txt`).addClass("future")
+      }
 }
